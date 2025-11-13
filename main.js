4f95a691e57e6003297851ab5bd4e276d6e8a6cb
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-13 20:04:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  res.sendDate("Hello world!")
   // TODO: Send the ""Hello, world!"" response
 });
 
 server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
   // TODO: Log a message to the console when the server starts listening on the port
 });
 
